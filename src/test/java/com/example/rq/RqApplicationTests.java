2830053e5ff78dd77f172761be8c72709ae1dbd5
Coder-RSL <a13036923860@163.com>
2023-03-22 10:06:14 +0800

    Initial commit

---
+++ src/test/java/com/example/rq/RqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
